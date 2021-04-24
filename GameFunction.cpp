9236e2f75b429b3e235d395a99544d4b28e79781
JeffreyL-hkucrypto <80671844+JeffreyL-hkucrypto@users.noreply.github.com>
2021-04-25 00:37:26 +0800

    Update GameFunction.cpp

--- GameFunction.cpp
+++ GameFunction.cpp
@@ -12,7 +12,7 @@ vector <player> players;
 void GetPlayer(int i){
   for (int j=0;j<i;j++){
     player i ;
-    i.money=2000;
+    i.money=150000;
     i.steps=0;
     i.position=0;
     players.push_back(i);
