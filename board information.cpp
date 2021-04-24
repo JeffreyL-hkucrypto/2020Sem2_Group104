a83815642dbbd9629cd146ceceb034061d93e40f
lkcsamlam <79414341+lkcsamlam@users.noreply.github.com>
2021-04-24 19:02:43 +0800

    Create board information.cpp

---
+++ board information.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+vector<string> board;
+board.push_back("Start Point");
+board.push_back("Tin Shui Wai");
+board.push_back("Community Chest");
+board.push_back("Cheung Chau");
+board.push_back("Income Tax");
+board.push_back("Tung Chung Station");
+board.push_back("Ap Lei Chau");
+board.push_back("Change");
+board.push_back("Kwun Tong");
+board.push_back("Lam Tin");
+board.push_back("Just Visting");
+board.push_back("Tuen Mun");
+board.push_back("Hong Kong electric");
+board.push_back("Yuen Long");
+board.push_back("Tai Po");
+board.push_back("Tsing Yi Station");
+board.push_back("Tai Wai");
+board.push_back("Community Chest");
+board.push_back("Sha Tin");
+board.push_back("Ma On Shan");
+board.push_back("Free Parking");
+board.push_back("start_point");
+board.push_back("start_point");
+board.push_back("start_point");
+board.push_back("start_point");
+board.push_back("start_point");
+board.push_back("start_point");
+board.push_back("start_point");
