be629571c5ce7aff5c2c2ebd9f380fc514c09ab9
JeffreyL-hkucrypto <80671844+JeffreyL-hkucrypto@users.noreply.github.com>
2021-04-24 22:13:01 +0800

    Update and rename printbox.h to print.h

---
+++ print.h
@@ -0,0 +1,5 @@
+#ifndef MONOPOLY_PRINT_H
+#define MONOPOLY_PRINT_H
+void change_sq_print(char line[], char sq[][10]);
+void print_box();
+#endif //MONOPOLY_PRINT_H
--- printbox.h
+++
@@ -1,4 +0,0 @@
-#ifndef MONOPOLY_PRINTBOX_H
-#define MONOPOLY_PRINTBOX_H
-void change_sq_print(char line[], char sq[][10]);
-#endif //MONOPOLY_PRINTBOX_H
