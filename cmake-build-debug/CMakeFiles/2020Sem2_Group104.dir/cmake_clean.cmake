c63d328a15a2c8147928f8e03b7e9216c09896a9
LeungCY <u3582154@connect.hku.hk>
2021-04-25 19:44:33 +0800

    Update board information.cpp

---
+++ cmake-build-debug/CMakeFiles/2020Sem2_Group104.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "2020Sem2_Group104.exe"
+  "2020Sem2_Group104.exe.manifest"
+  "2020Sem2_Group104.lib"
+  "2020Sem2_Group104.pdb"
+  "CMakeFiles/2020Sem2_Group104.dir/GameFunction.cpp.obj"
+  "CMakeFiles/2020Sem2_Group104.dir/board_information.cpp.obj"
+  "CMakeFiles/2020Sem2_Group104.dir/main.cpp.obj"
+  "CMakeFiles/2020Sem2_Group104.dir/print.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2020Sem2_Group104.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
